3069eff81b27828bfc8659e96d51281ab9f577bf
MichaelSZM <michaelszm@163.com>
2015-05-19 16:53:42 +0800

    Michael
    first work how to use log

---
+++ Tz_Michael/第一次作业（Log与Debug）/TaskOne/src/com/tz/taskone/TaskOneActivity.java
@@ -0,0 +1,69 @@
+package com.tz.taskone;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tz.utils.LogUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class TaskOneActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void getLog(View v){
+    	LogUtils.isDebug=true;//开启log
+    	LogUtils.w("warn", "this is an warnning info");
+    	LogUtils.e("error", "this is an error info");
+    	LogUtils.println(Log.WARN, "warn", "this is an warnning info by println");
+    	LogUtils.println(Log.ERROR, "error", "this is an error info by println");
+    	StringBuffer buffer=new StringBuffer();
+    	buffer.append(getLogWarnOrError("WARN"));
+    	buffer.append("\n");
+    	buffer.append(getLogWarnOrError("ERROR"));
+    	Toast.makeText(this, buffer.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 获取系统log ，并通过toast显示
+     */
+	private String getLogWarnOrError(String selector) {
+		StringBuffer buffer=new StringBuffer();
+		List<String> cmdLine=new ArrayList<String>();
+		cmdLine.add("logcat");
+		cmdLine.add("-d");//只取一次log
+		cmdLine.add("-s");//过滤
+		cmdLine.add(selector);
+		try {
+			Process exec=Runtime.getRuntime().exec(cmdLine.toArray(new String[cmdLine.size()]));
+			InputStream in=exec.getInputStream();
+			InputStreamReader reader=new InputStreamReader(in);
+			BufferedReader bReader=new BufferedReader(reader);
+			String temp=null;
+			while((temp=bReader.readLine())!=null){
+				buffer.append(temp);
+				buffer.append("\n");
+			}
+			if(buffer.toString().trim().length()!=0){
+				return buffer.toString();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "";
+	}
+    
+}
---
+++ Tz_Michael/第一次作业（Log与Debug）/TaskOne/src/com/tz/utils/LogUtils.java
@@ -0,0 +1,79 @@
+package com.tz.utils;
+
+import android.util.Log;
+/**
+ * log工具类，可以通过设置LogUtils.isDebug来动态的设置是否开启log
+ * @author szm
+ *
+ */
+public class LogUtils {
+
+	/**定义log的开关*/
+	public static boolean isDebug;
+	
+	/**
+	 * info级别的log
+	 * @param tag 过滤条件
+	 * @param msg 日志信息
+	 * @return int 0失败  其他 成功
+	 */
+	public static int i(String tag,String msg){
+		if(isDebug){
+			return Log.i(tag, msg);
+		}
+		return 0;
+	}
+	
+	/**
+	 * error 级别的log
+	 * @param tag 过滤条件
+	 * @param msg 日志信息
+	 * @return int 0失败  其他 成功
+	 */
+	public static int e(String tag,String msg){
+		if(isDebug){
+			return Log.e(tag, msg);
+		}
+		return 0;
+	}
+	
+	/**
+	 * WARN 级别 
+	 * @param tag 过滤条件
+	 * @param msg 日志信息
+	 * @return int 0失败  其他 成功
+	 */
+	public static int w(String tag,String msg){
+		if(isDebug){
+			return Log.w(tag, msg);
+		}
+		return 0;
+	}
+	
+	/**
+	 * debug 级别
+	 * @param tag 过滤条件
+	 * @param msg 日志信息
+	 * @return int 0失败  其他 成功
+	 */
+	public static int d(String tag,String msg){
+		if(isDebug){
+			return Log.d(tag, msg);
+		}
+		return 0;
+	}
+	
+	/**
+	 * 通用log方法
+	 * @param priority 信息级别（int）fg:Log.INFO etc
+	 * @param tag 过滤条件
+	 * @param msg 日志信息
+	 * @return int 0失败  其他 成功
+	 */
+	public static int println(int priority,String tag,String msg){
+		if(isDebug){
+			return Log.println(priority, tag, msg);
+		}
+		return 0;
+	}
+}
