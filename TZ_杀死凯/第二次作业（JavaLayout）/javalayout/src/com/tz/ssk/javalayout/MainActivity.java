5b49039fbd7ea669955a59e64a743d9f6509347a
565204031 <565204031@qq.com>
2015-05-20 15:06:56 +0800

    第二次作业修改
    第二次作业修改

--- TZ_杀死凯/第二次作业（JavaLayout）/javalayout/src/com/tz/ssk/javalayout/MainActivity.java
+++ TZ_杀死凯/第二次作业（JavaLayout）/javalayout/src/com/tz/ssk/javalayout/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 
@@ -21,11 +23,20 @@ public class MainActivity extends Activity {
         //setContentView(R.layout.activity_main);
         mContext=this;
         
+        
         //LinearLayout 初始化
         LinearLayout ll=new LinearLayout(mContext);
-        ll.setOrientation(LinearLayout.HORIZONTAL);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setBackgroundResource(R.color.white);
         LayoutParams ll_lp=new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         ll.setLayoutParams(ll_lp);
+        
+        //LinearLayout 初始化
+        LinearLayout ll_top=new LinearLayout(mContext);
+        ll_top.setOrientation(LinearLayout.HORIZONTAL);
+        ll_top.setBackgroundResource(R.color.blue);
+        LayoutParams ll_toplp=new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        ll_top.setLayoutParams(ll_toplp);
         
         //Button 初始化
         Button bt=new Button(mContext);
@@ -39,8 +50,26 @@ public class MainActivity extends Activity {
         et.setLayoutParams(et_lp);
         
         
-        ll.addView(et, 0);
-        ll.addView(bt, 1);
+        //LinearLayout 初始化
+        LinearLayout ll_middle=new LinearLayout(mContext);
+        ll_middle.setGravity(Gravity.CENTER);
+        LayoutParams ll_middlelp=new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        ll_middle.setLayoutParams(ll_middlelp);
+        ll_middle.setBackgroundResource(R.color.yellow);
+        
+        
+        //ImageView 初始化
+        ImageView iv=new ImageView(mContext);
+        LayoutParams iv_lp=new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        iv.setLayoutParams(iv_lp);
+        iv.setImageResource(R.drawable.user);
+        
+        
+        ll_top.addView(et, 0);
+        ll_top.addView(bt, 1);
+        ll_middle.addView(iv, 0);
+        ll.addView(ll_top,0);
+        ll.addView(ll_middle,1);
         setContentView(ll);
     }
 
