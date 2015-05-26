85f5e39de649ff4f27a1cc39457eedff55fc31c7
tz008dave <tz008dave@163.com>
2015-05-26 13:49:11 +0800

    第四次作业

---
+++ Tz_淡然一笑/第四次作业（高级控件与适配器）/tz_custom_adapter/src/com/tz/customadapter/bean/Student.java
@@ -0,0 +1,53 @@
+package com.tz.customadapter.bean;
+
+public class Student {
+	/**
+	 * 头像
+	 */
+	private int faceImg;
+	/**
+	 * 网名
+	 */
+	private String netName;
+	/**
+	 * 性别  
+	 */
+	private String sex;
+	/**
+	 * 爱好
+	 */
+	private String hobby;
+
+	public int getFaceImg() {
+		return faceImg;
+	}
+
+	public void setFaceImg(int faceImg) {
+		this.faceImg = faceImg;
+	}
+
+	public String getNetName() {
+		return netName;
+	}
+
+	public void setNetName(String netName) {
+		this.netName = netName;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+}
