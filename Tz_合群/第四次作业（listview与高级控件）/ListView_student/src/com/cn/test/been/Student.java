ed28b4a5333877625c1484625b674a7e00af4e65
hequnsky <1363790125@qq.com>
2015-05-27 08:31:57 +0800

    第四次作业同步
    第四次作业第一次提交

---
+++ Tz_合群/第四次作业（listview与高级控件）/ListView_student/src/com/cn/test/been/Student.java
@@ -0,0 +1,55 @@
+package com.cn.test.been;
+
+/**
+ * Created on 2015年5月25日上午11:48:16 Student.java<br/>
+ * Author ZhuHequn 1363790125@qq.com <br/>
+ * version 1.0 TODO
+ */
+public class Student {
+	private String Stu_name;
+	private String stu_color;
+	private String stu_sex;
+	private String stu_hobby;
+	private int stu_face;
+
+	public String getStu_name() {
+		return Stu_name;
+	}
+
+	public void setStu_name(String stu_name) {
+		Stu_name = stu_name;
+	}
+
+	public String getStu_color() {
+		return stu_color;
+	}
+
+	public void setStu_color(String stu_color) {
+		this.stu_color = stu_color;
+	}
+
+	public String getStu_sex() {
+		return stu_sex;
+	}
+
+	public void setStu_sex(String stu_sex) {
+		this.stu_sex = stu_sex;
+	}
+
+	public String getStu_hobby() {
+		return stu_hobby;
+	}
+
+	public void setStu_hobby(String stu_hobby) {
+		this.stu_hobby = stu_hobby;
+	}
+
+	public int getStu_face() {
+		return stu_face;
+	}
+
+	public void setStu_face(int stu_face) {
+		this.stu_face = stu_face;
+	}
+
+}
