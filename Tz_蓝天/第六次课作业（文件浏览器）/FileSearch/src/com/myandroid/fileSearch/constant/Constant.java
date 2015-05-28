179a46be16a9be693ccffb82c4a444cda07d7954
blueSkyWhiteClouds <794651565@qq.com>
2015-05-28 17:54:49 +0800

    TZ_蓝天_第六次课作业
    TZ_蓝天_第六次课作业

---
+++ Tz_蓝天/第六次课作业（文件浏览器）/FileSearch/src/com/myandroid/fileSearch/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.myandroid.fileSearch.constant;
+
+import android.os.Environment;
+
+public class Constant {
+	//获取sd卡的绝对路径
+	public final static String PATH=Environment.getExternalStorageDirectory().getAbsolutePath();
+}
