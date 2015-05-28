8220f17498a7b75fd8b64b786cfd9630bddc8c63
xugelaji <361911854@qq.com>
2015-05-28 12:00:03 +0800

    Revert "Revert "第四次作业""
    This reverts commit 7a22e784fbcf7081ccb73f174b7567b86d7a8ce8.

---
+++ TZ_陈潜/第四次作业/DemoListView/src/com/example/demolistview/adapter/bean/Star.java
@@ -0,0 +1,39 @@
+package com.example.demolistview.adapter.bean;
+
+public class Star {
+	private String name;
+	private String sex;
+	private int    head;
+	private String face;
+	private String hoobby;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getHead() {
+		return head;
+	}
+	public void setHead(int head) {
+		this.head = head;
+	}
+	public String getFace() {
+		return face;
+	}
+	public void setFace(String face) {
+		this.face = face;
+	}
+	public String getHoobby() {
+		return hoobby;
+	}
+	public void setHoobby(String hoobby) {
+		this.hoobby = hoobby;
+	}
+}
