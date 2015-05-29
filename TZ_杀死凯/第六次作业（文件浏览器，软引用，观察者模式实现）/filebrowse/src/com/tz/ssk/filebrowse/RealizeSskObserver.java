b8e07020d4bd01a43771466604f7183ae087e3f4
565204031 <565204031@qq.com>
2015-05-29 15:48:33 +0800

    第六次作业(文件浏览器，软引用，观察者模式实现)
    第六次作业(文件浏览器，软引用，观察者模式实现)

---
+++ TZ_杀死凯/第六次作业（文件浏览器，软引用，观察者模式实现）/filebrowse/src/com/tz/ssk/filebrowse/RealizeSskObserver.java
@@ -0,0 +1,13 @@
+package com.tz.ssk.filebrowse;
+
+import android.util.Log;
+
+public class RealizeSskObserver extends SskObserver{
+
+	@Override
+	public void OnChanged() {
+		// TODO Auto-generated method stub
+		Log.i("RealizeSskObserver", "各位同学，我观察到了");
+	}
+	
+}
