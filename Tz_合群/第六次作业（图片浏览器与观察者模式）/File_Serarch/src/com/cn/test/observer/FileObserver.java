c49f4879517239e72803c508d97a79f561c2d0d9
hequnsky <1363790125@qq.com>
2015-06-01 15:33:25 +0800

    第六次作业第一次提交
    第六次作业第一次提交

---
+++ Tz_合群/第六次作业（图片浏览器与观察者模式）/File_Serarch/src/com/cn/test/observer/FileObserver.java
@@ -0,0 +1,16 @@
+package com.cn.test.observer;
+
+/**
+ * Created on2015-5-30 обнГ2:50:24 FileObserver.java<br/>
+ * Author ZhuHequn 1363790125@qq.com <br/>
+ * version 1.0 TODO
+ */
+public class FileObserver extends Observer {
+
+	@Override
+	public void onChanged() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
