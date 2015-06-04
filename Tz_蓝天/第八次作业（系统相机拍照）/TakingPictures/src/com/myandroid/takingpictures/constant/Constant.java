a51a91942a217e61d684157cba76049d15181d28
blueSkyWhiteClouds <794651565@qq.com>
2015-06-04 20:23:45 +0800

    Revert "TZ_蓝天_6月4日_第八次作业（系统相机拍照）"
    This reverts commit 14f3c040e583de044a262e2045623c2278bbae62.

--- Tz_蓝天/第八次作业（系统相机拍照）/TakingPictures/src/com/myandroid/takingpictures/constant/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.myandroid.takingpictures.constant;
-
-import android.os.Environment;
-
-public class Constant {
-	
-	public final static String PATH=Environment.getExternalStorageDirectory().getAbsolutePath();
-}
--- Tz_蓝天/第八次作业（系统相机拍照）/TakingPictures/src/com/myandroid/takingpictures/util/PicturePath.java
+++
@@ -1,9 +0,0 @@
-package com.myandroid.takingpictures.util;
-
-public class PicturePath {
-
-	public void picPath(String path)
-	{
-		
-	}
-}
