89174a92863f979c7c67e12dfde96bbc5f6a2066
limz2004 <1048375146@qq.com>
2015-06-04 19:55:24 +0800

    第八次作业
    Intent显式和隐式跳转

---
+++ Tz_世界末日/IntentDemo/src/com/limz/intentdemo/activity/TwoActivity.java
@@ -0,0 +1,14 @@
+package com.limz.intentdemo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TwoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	
+}
