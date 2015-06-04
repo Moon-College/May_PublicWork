86d382a099febb44155cfbf3c0070a552f47a38b
dongdongdong1 <517842198@qq.com>
2015-06-04 20:24:39 +0800

    第八次作业

---
+++ Tz_丁丁/SixTwoHomeWork/src/com/dd/sixtwohomework/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.dd.sixtwohomework;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_second);
+	}
+}
