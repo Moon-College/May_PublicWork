81e8ebb0e498e9ac270af266aa0fb41848f1d962
LZQL <396298154@qq.com>
2015-06-05 22:58:41 +0800

    第九次作业

---
+++ TZ-LZQ/第九次作业（通过反射获取控件的值）/10_Reflection_test/src/com/lzq/reflect/bean/Student.java
@@ -0,0 +1,25 @@
+package com.lzq.reflect.bean;
+
+public class Student {
+
+	private String name = "ÑþÑþ";
+	private String city;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+
+}
