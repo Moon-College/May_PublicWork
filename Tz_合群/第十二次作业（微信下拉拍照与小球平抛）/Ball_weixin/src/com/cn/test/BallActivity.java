6e353259e21c5afac4fbaca38dabedf8f39dbf37
hequnsky <1363790125@qq.com>
2015-06-19 12:17:05 +0800

    第十二次作业第一次提交
    第十二次作业第一次提交

---
+++ Tz_合群/第十二次作业（微信下拉拍照与小球平抛）/Ball_weixin/src/com/cn/test/BallActivity.java
@@ -0,0 +1,94 @@
+package com.cn.test;
+
+import com.cn.test.utils.Reflection;
+
+import android.app.ActionBar.LayoutParams;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created on2015-6-19 ÉÏÎç9:23:27 BallActivity.java<br/>
+ * Author ZhuHequn 1363790125@qq.com <br/>
+ * version 1.0 TODO
+ */
+public class BallActivity extends Activity implements OnClickListener {
+	private LinearLayout ll;
+	private Button start;
+	private float x = 0, y = 0;
+	private ImageView img;
+	private LayoutParams lp;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		Reflection.initView(this);
+		initView();
+		start.setOnClickListener(this);
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		Bitmap bitmap = Bitmap.createBitmap(120, 120, Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		Paint paint = new Paint();
+		paint.setColor(Color.BLUE);
+		paint.setAntiAlias(true);
+		canvas.drawCircle(60, 20, 20, paint);
+
+		img = new ImageView(this);
+		img.setBackgroundDrawable(new BitmapDrawable(bitmap));
+		lp = new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+		lp.gravity = Gravity.BOTTOM;
+		img.setLayoutParams(lp);
+		ll.addView(img);
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+}
+// @Override
+// protected void onCreate(Bundle savedInstanceState) {
+// super.onCreate(savedInstanceState);
+// setContentView(R.layout.myball);
+// root = (LinearLayout) findViewById(R.id.root);
+// start = (Button) findViewById(R.id.start);
+//
+// init();
+//
+// start.setOnClickListener(new OnClickListener() {
+// @Override
+// public void onClick(View v) {
+// valueAnimator.start();
+// valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+// @Override
+// public void onAnimationUpdate(ValueAnimator animation) {
+// float progress = Float.parseFloat(animation.getAnimatedValue().toString());
+// x = (int) (progress * 50) + 20;
+// y = (int) Math.sqrt(x);
+// if( x != 20) {
+// iv.setX(x);
+// iv.setY(y>0?y * 50:-y * 50);
+// }
+// }
+// });
+// }
+// });
+
+//
+// }
