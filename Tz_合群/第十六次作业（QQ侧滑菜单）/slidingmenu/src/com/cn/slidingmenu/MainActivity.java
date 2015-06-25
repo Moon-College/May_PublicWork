748cae9c9f4598ff7a9fdda5eb8d382eb2ba0565
hequnsky <1363790125@qq.com>
2015-06-25 19:22:18 +0800

    第十六次作业第一次同步
    第十六次作业第一次同步

---
+++ Tz_合群/第十六次作业（QQ侧滑菜单）/slidingmenu/src/com/cn/slidingmenu/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cn.slidingmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.slidingmenu.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
