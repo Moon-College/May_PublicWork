f74c126ebe4974d34350af0954e2b522189e6336
Moon-Collage-Android <wjky11111@163.com>
2015-07-07 17:10:13 +0800

    1 six

---
+++ Tz_keven_e/第六次作业/Damplist/src/com/keven/customview/activity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.keven.customview.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+       @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	// TODO Auto-generated method stub
+    	super.onCreate(savedInstanceState);
+    	//setContentView(R.id.)
+    }
+}
