682051fd63868f0f0f3bd6082204114ebe225cc9
zhanght <275286413@qq.com>
2015-07-09 20:38:33 +0800

    第19，20，21，22次作业

---
+++ TZ_空杯/第二十、二十一次课_高级自定义控件/lsn20_21_priceview/src/com/zht/priceview/MainActivity.java
@@ -0,0 +1,32 @@
+/**
+ * Project Name:lsn20_21_priceview
+ * File Name:MainActivity.java
+ * Package Name:com.zht.priceview
+ * Date:2015-7-6обнГ4:51:03
+ * Copyright (c) 2015, shixi_hongtao@staff.sina.com.cn All Rights Reserved.
+ *
+ */
+
+package com.zht.priceview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * ClassName:MainActivity <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2015-7-6 обнГ4:51:03 <br/>
+ * 
+ * @author acer
+ * @version
+ * @since JDK 1.6
+ * @see
+ */
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
