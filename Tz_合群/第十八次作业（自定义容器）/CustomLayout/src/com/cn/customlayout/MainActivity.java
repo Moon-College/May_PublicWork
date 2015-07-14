6953655330446a4065fba645b6b44b2af6f40c14
hequnsky <1363790125@qq.com>
2015-07-14 11:47:37 +0800

    第十八次作业
    第十八次作业第一次同步

---
+++ Tz_合群/第十八次作业（自定义容器）/CustomLayout/src/com/cn/customlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cn.customlayout;
+
+import com.cn.customlayout.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
