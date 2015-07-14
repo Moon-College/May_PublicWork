1d2678299b56180719e5ab506fa48a566664a6d1
hequnsky <1363790125@qq.com>
2015-07-14 22:04:02 +0800

    测试提交
    测试提交

---
+++ Tz_合群/测试/News/src/com/example/news/NewsAdpter.java
@@ -0,0 +1,12 @@
+package com.example.news;
+
+import java.util.List;
+
+import android.content.Context;
+
+public class NewsAdpter  {
+	public NewsAdpter(Context context,int textViewResourceId,List<News>objects){
+		
+
+	}
+}
