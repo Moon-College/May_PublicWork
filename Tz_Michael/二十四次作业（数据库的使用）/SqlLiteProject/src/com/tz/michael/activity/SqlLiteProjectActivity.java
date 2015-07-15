008bc0cd1d522edd8e81aa2452a65aa0d0e8030b
MichaelSZM <michaelszm@163.com>
2015-07-15 15:04:02 +0800

    Michael
    通用的数据库操作方法

--- Tz_Michael/二十四次作业（数据库的使用）/SqlLiteProject/src/com/tz/michael/activity/SqlLiteProjectActivity.java
+++ Tz_Michael/二十四次作业（数据库的使用）/SqlLiteProject/src/com/tz/michael/activity/SqlLiteProjectActivity.java
@@ -2,6 +2,7 @@ package com.tz.michael.activity;
 
 import java.util.List;
 
+import com.tz.michael.bean.ClassBean;
 import com.tz.michael.bean.Student;
 import com.tz.michael.utils.DBHelper;
 import com.tz.michael.utils.MySqlLiteHelper;
@@ -21,10 +22,26 @@ public class SqlLiteProjectActivity extends Activity {
         setContentView(R.layout.main);
         helper=new MySqlLiteHelper(this, "school", null, 1);
         db=helper.getWritableDatabase();
-        List<Student> s=DBHelper.queryData(db, "students", "学生3");
-        Log.i("ss--", s.toString());
-        List<Student> sl=DBHelper.queryStudentsByClassName(db, "班级1");
-        Log.i("sl--", sl.toString());
+//        List<Student> s=DBHelper.queryData(db, "students", "学生3");
+//        Log.i("ss--", s.toString());
+//        List<Student> sl=DBHelper.queryStudentsByClassName(db, "班级1");
+//        Log.i("sl--", sl.toString());
+        Student student=new Student("szm", 24, 1);
+//        DBHelper.queryDataTest(student);
+//        DBHelper.insertData(db, student, "students");
+        ClassBean classBean=new ClassBean("vip7月", 20);
+        DBHelper.insertData(db, classBean, "class");
+//        List<Student> s_in=DBHelper.queryData(db, "students", "szm");
+//        Log.i("ins--", s_in.toString());
+        DBHelper.updateData(db, "students", student, 12);
+        DBHelper.deleteData(db, "students", 11);
+        Student student2=(Student) DBHelper.queryData(db, "students", Student.class, 14);
+        Log.i("quer--", student2.toString());
+        List<Object> ls = DBHelper.queryDataByName(db, "students", Student.class, "szm");
+        for(Object object:ls){
+        	Student student3=(Student) object;
+        	Log.i("llqq--", student3.toString());
+        }
     }
     
     @Override
